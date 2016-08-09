92b3c880a4d20c71761accb7b12906c77072eb22
Dobrik <dobrik1990@gmail.com>
2016-08-09 12:26:08 +0300

    autoload classes

--- config/main.php
+++ config/main.php
@@ -2,7 +2,13 @@
 session_start();
 
 include_once 'db.php';
-include_once '/../Class.User.php';
+
+function loadClasses($class)
+{
+    include_once '/../Classes/Class.'.$class.'.php';
+}
+spl_autoload_register('loadClasses');
+
 $user = new User($link);
 
 //if($user->register('dobrik', '1234567', '1234567', 'dobrik1990@gmail.com')){
